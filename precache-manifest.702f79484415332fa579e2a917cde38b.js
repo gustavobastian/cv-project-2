0f6d0d3cba0bf3a2cdce506f3dc83a3047a4b646
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-03 20:59:28 -0300

    adding build

---
+++ precache-manifest.702f79484415332fa579e2a917cde38b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f24506c9aa915603a81e",
+    "url": "/gustavobastian/cv-project/static/css/main.2f0c9dd6.chunk.css"
+  },
+  {
+    "revision": "f24506c9aa915603a81e",
+    "url": "/gustavobastian/cv-project/static/js/main.f24506c9.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/gustavobastian/cv-project/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "8deb6ff425498aaffd15",
+    "url": "/gustavobastian/cv-project/static/js/2.8deb6ff4.chunk.js"
+  },
+  {
+    "revision": "a8079bacaa2d5dd93207",
+    "url": "/gustavobastian/cv-project/static/js/runtime~main.a8079bac.js"
+  },
+  {
+    "revision": "2e7b3f20aa732d6049a4eacfa1c357d5",
+    "url": "/gustavobastian/cv-project/static/media/face.2e7b3f20.jpeg"
+  },
+  {
+    "revision": "cdf395442d35f8b96e2b1fa0902fd089",
+    "url": "/gustavobastian/cv-project/index.html"
+  }
+];
