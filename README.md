0aeacf4e18fe4092a7f2ed3c4b16a5010b8bedac
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-07 14:16:09 -0300

    using useState in education Element

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Getting Started with React , simple cv form for the Odin Project
 
-More info: <https://www.theodinproject.com/lessons/node-path-javascript-cv-application>
+This project is developed using functional components and hooks.
+
+More info: <https://www.theodinproject.com/lessons/node-path-javascript-hooks>
--- src/components/educationElement.js
+++ src/components/educationElement.js
@@ -1,100 +1,88 @@
-import React, { Component } from 'react'
+import React, { Component,useState } from 'react'
 import '../styles/educationElement.css';
 
-class educationElement extends Component {
-    constructor(props) {
-        super(props)
-        
 
-        this.state={            
+const educationElement = props=>{
+        const [state,setState]=useState({            
             universityName:props.university,
             title:props.title,
             dateEnd:props.dateEnd,
             dateEntry:props.dateEntry,
             note:props.Note,
+        })
+        
 
-        }
-
-        this.handleClickNoteEducation=this.handleClickNoteEducation.bind(this);
-        this.handleClickLabelEducation=this.handleClickLabelEducation.bind(this);
-        this.handleClickTitleEducation=this.handleClickTitleEducation.bind(this);
-        this.handleClickDateEntry=this.handleClickDateEntry.bind(this);
-        this.handleClickDateEnd=this.handleClickDateEnd.bind(this);
-
-    }
-     
-
-    handleClickDateEnd(){
+    const handleClickDateEnd=()=>{
         let response=prompt("DateEnd:",this.state.dateEnd);
         if((response==="")||response===null){
             console.log("here")}
         else{
-            this.setState({
+            setState({
                 dateEnd:parseInt(response),
             });
         } 
        
     };
 
-    handleClickDateEntry(){
-        let response=prompt("DateEntry:",this.state.dateEntry);
+    const handleClickDateEntry=()=>{
+        let response=prompt("DateEntry:",state.dateEntry);
         if((response==="")||response===null){
             console.log("here")}
         else{
-            this.setState({
+            setState({
                 dateEntry:parseInt(response),
             });
         } 
        
     };
 
-    handleClickNoteEducation(){
-        let response=prompt("Note:",this.state.note);
+    const handleClickNoteEducation=()=>{
+        let response=prompt("Note:",state.note);
         if((response==="")||response===null){
             console.log("here")}
         else{
-            this.setState({
+            setState({
                 note:response,
             });
         }        
     };
 
-    handleClickLabelEducation(){
-        let response=prompt("University/Company:",this.state.universityName);
+    const handleClickLabelEducation=()=>{
+        let response=prompt("University/Company:",state.universityName);
         if((response==="")||response===null){
             console.log("here")}
         else{
-            this.setState({
+            setState({
                 universityName:response,
             });
         }        
     };
-    handleClickTitleEducation(){
-        let response=prompt("Title/Degree:",this.state.title);
+    const handleClickTitleEducation=()=>{
+        let response=prompt("Title/Degree:",state.title);
         if((response==="")||response===null){
             console.log("here")}
         else{
-            this.setState({
+            setState({
                 title:response,
             });
         }        
     };
 
-    render() {
+    
         return (
             <div>
             <div className="educationElement">            
-            <div className="labelEducation" onClick={this.handleClickLabelEducation}>{this.state.universityName}</div>
-            <div className="titleEducation" onClick={this.handleClickTitleEducation}> {this.state.title}</div>
-            <div className="datesEducation" onClick={this.handleClickDateEntry} > {this.state.dateEntry}/</div>
-            <div className="datesEducation" onClick={this.handleClickDateEnd} >{this.state.dateEnd}</div>
+            <div className="labelEducation" onClick={handleClickLabelEducation.bind(this,'')}>{state.universityName}</div>
+            <div className="titleEducation" onClick={handleClickTitleEducation.bind(this,'')}> {state.title}</div>
+            <div className="datesEducation" onClick={handleClickDateEntry.bind(this,'')} > {state.dateEntry}/</div>
+            <div className="datesEducation" onClick={handleClickDateEnd.bind(this,'')} >{state.dateEnd}</div>
             </div>
-            <div className="Note" onClick={this.handleClickNoteEducation}> Note: {this.state.note}</div>
+            <div className="Note" onClick={handleClickNoteEducation}> Note: {state.note}</div>
 
             
             </div>
         )
-    }
+    
 }
 
 
